8817da289e902a8513d127c584a8b90bff1ebeda
rohith <sairohith@msupply.com>
2017-11-06 08:39:39 +0530

    changed

--- index.js
+++ index.js
@@ -6,11 +6,37 @@ var dbConfigValidation = require('./validation.js').dbConfigValidation;
 var util = require('./util.js');
 
 exports.getIPDetails = (inputDbConfig, ip, cb) => {
-    if (!ip || ip.toString().trim().length == 0)
-        return cb(true, "Invalid IP");
-    getIPDetails1.getIPDetails(inputDbConfig,ip, (err, res) => {
-        return cb(err, res);
-    });
+    console.log("isnide getIPDetails");
+    var validation_result = validator.validate(dbConfig, dbConfigValidation);
+
+    var errors = [];
+
+    if (validation_result.errors.length > 0) {
+        console.log(1);
+        errors = util.makeErrorObject(validation_result.errors);
+        console.log(11);
+        console.log(errors);
+        console.log(12);
+    }
+
+    if (!ip || ip.toString().trim().length == 0) {
+        console.log(2);
+
+        errors.push("Invalid IP");
+    }
+    console.log(3);
+    if (errors.length > 0) {
+        console.log(4);
+
+        console.log("errors");
+        return cb(true, errors)
+    } else {
+        console.log(5);
+
+        getIPDetails1.getIPDetails(inputDbConfig, ip, (err, res) => {
+            return cb(err, res);
+        });
+    }
 };
 
 exports.importData = (dbConfig, filePath, cb) => {
